4fe6e4de9c5fb30250e26c0df9a055850136ee4b
Everson Alves de Rezende <rezende.everson@gmail.com>
2022-01-27 21:33:51 -0300

    usando supabase como back

--- pages/chat.js
+++ pages/chat.js
@@ -1,11 +1,30 @@
 import { Box, Text, TextField, Image, Button } from '@skynexui/components'
 import React from 'react'
 import appConfig from '../config.json'
+import { createClient } from '@supabase/supabase-js'
+
+// COMO FAZER AJAX: https://medium.com/@omariosouto/entendendo-como-fazer-ajax-com-a-fetchapi-977ff20da3c6
+const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzMyNTg2NCwiZXhwIjoxOTU4OTAxODY0fQ.kY7qhH4CPDSR93RqEPIKtOLlmoxMihcCMpLbzwEjn3A'
+const SUPABASE_URL = 'https://pullfdptqeowqlxbaiop.supabase.co'
+
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 export default function ChatPage() {
   // Sua lógica vai aqui
   const [mensagem, setMensagem] = React.useState('')
   const [listaDeMensagens, setListaDeMensagens] = React.useState([])
+
+  React.useEffect(() => {
+    supabaseClient
+      .from('mensagens')
+      .select('*')
+      .order('id', { ascending: false })
+      .then(({ data }) => {
+        console.log('Dados da consulta: ', data)
+        setListaDeMensagens(data)
+      })
+  }, [])
 
   /*
     Usuário
@@ -14,20 +33,31 @@ export default function ChatPage() {
     - Tem que adicionar o texto na lista
 
     Dev
-    - [ ] Campo criado
-    - [ ] Vamos usar o onChange que usa o useState (ter um if para caso seja enter para Limpar a variável)
-    - [ ] Lista de Mensagem
+    - [X] Campo criado
+    - [X] Vamos usar o onChange que usa o useState (ter um if para caso seja enter para Limpar a variável)
+    - [X] Lista de Mensagem
   */
 
   // ./Sua lógica vai aqui
 
   function handleNovaMensagem(novaMensagem) {
     const mensagem = {
-      id: listaDeMensagens.length + 1,
+      //id: listaDeMensagens.length + 1,
       de: 'eversonrezende',
       texto: novaMensagem
     }
-    setListaDeMensagens([mensagem, ...listaDeMensagens])
+
+    supabaseClient
+      .from('mensagens')
+      .insert([
+        // Tem que ser um objeto com os MESMOS CAMPOS que você escreveu no supabase
+        mensagem
+      ])
+      .then(({ data }) => {
+        console.log('Criando mensagem: ', data)
+      })
+
+    setListaDeMensagens([data[0], ...listaDeMensagens])
     setMensagem('')
   }
 
@@ -186,7 +216,7 @@ function MessageList(props) {
                   display: 'inline-block',
                   marginRight: '8px'
                 }}
-                src={`https://github.com/eversonrezende.png`}
+                src={`https://github.com/${mensagem.de}.png`}
               />
               <Text tag="strong">{mensagem.de}</Text>
               <Text
